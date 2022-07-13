de7ff94b5cf0517d5e89916fe9d5882fea282029
SashaH62 <sashahermanwebdev@gmail.com>
2022-07-13 19:58:26 +0200

    Create call back functions for removing class and playing sound on keypress

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-// Adding event listener to window for keypress
-window.addEventListener("keydown", function (event) {
-  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+//  Function to play sound on key press
+
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+
+  // Assigning currently pressed key to variable for adding class
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
   // If unassigned key is pressed, end function
-  if (!audio) {
-    return;
-  }
+  if (!audio) return;
+
   // Reset audio to beginning to allow for constant keypress
   audio.currentTime = 0;
   audio.play();
-  // Assigning currently pressed key to variable for adding class
-  const key = document.querySelector(`[data-key="${event.keyCode}"]`);
   key.classList.add("playing");
-});
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
 
 const keys = document.querySelectorAll(".key");
-keys.forEach((key) => {
-  key.addEventListener("transitionend", () => {
-    key.classList.remove("playing");
-  });
-});
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+
+// Adding event listener to window for keypress
+window.addEventListener("keydown", playSound);
