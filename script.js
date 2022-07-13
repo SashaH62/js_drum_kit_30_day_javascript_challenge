8d0c9ce6ab23588f964d24bdaf20b9e2d38f0bde
SashaH62 <sashahermanwebdev@gmail.com>
2022-07-13 19:39:54 +0200

    Change code for toggling class on key to make use of event listener

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+// Adding event listener to window for keypress
 window.addEventListener("keydown", function (event) {
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  // If unassigned key is pressed, end function
   if (!audio) {
     return;
   }
+  // Reset audio to beginning to allow for constant keypress
   audio.currentTime = 0;
   audio.play();
+  // Assigning currently pressed key to variable for adding class
   const key = document.querySelector(`[data-key="${event.keyCode}"]`);
   key.classList.add("playing");
-  setTimeout(function () {
+});
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => {
+  key.addEventListener("transitionend", () => {
     key.classList.remove("playing");
-  }, 150);
+  });
 });
